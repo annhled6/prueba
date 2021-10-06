58d4d8551753bfc9a92a9a460a40e80924dfcb6d
annhled6 <alhernandezledesma@liigh.unam.mx>
2021-10-06 13:28:34 -0500

    Prueba

--- Script.R
+++ Script.R
@@ -1,1 +1,13 @@
 ## PRUEBA GITHUB
+
+
+
+library("gitcreds")
+library("gert")
+library("gh")
+
+# inicializar el repositorio de Git
+usethis::use_git() #
+
+# conectar tu repositorio local de Git con los servidores de GitHub
+usethis::use_github()
